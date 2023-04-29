2146ebfa222df708849db9b302259ee58a247351
Lukas TAMAYO <luknight@hotmail.fr>
2023-04-29 16:44:40 +0200

    Fixing error

--- Features/Pipe.gd
+++ Features/Pipe.gd
@@ -13,25 +13,28 @@ extends Node2D
 
 # Controlling pipe flow
 func _input(event):
-	if event.is_action_type():
+	# Doing a bunch of create tween in order to avoid errors
+	if event.is_action_pressed("BlockLeft"):
 		var tween = create_tween()
-		if event.is_action_pressed("BlockLeft"):
-			left_blocker.show()
-			left_blocker_collision.disabled = false;
-			left_blocker.modulate = Color.TRANSPARENT
-			tween.tween_property(left_blocker, "modulate", Color.WHITE, tween_durations)
-		elif event.is_action_released("BlockLeft"):
-			left_blocker_collision.disabled = true;
-			left_blocker.modulate = Color.WHITE
-			tween.tween_property(left_blocker, "modulate", Color.TRANSPARENT, tween_durations)
-			tween.tween_callback(left_blocker.hide)
-		elif event.is_action_pressed("BlockRight"):
-			right_blocker.show()
-			right_blocker_collision.disabled = false;
-			right_blocker.modulate = Color.TRANSPARENT
-			tween.tween_property(right_blocker, "modulate", Color.WHITE, tween_durations)
-		elif event.is_action_released("BlockRight"):
-			right_blocker_collision.disabled = true;
-			right_blocker.modulate = Color.WHITE
-			tween.tween_property(right_blocker, "modulate", Color.TRANSPARENT, tween_durations)
-			tween.tween_callback(right_blocker.hide)
+		left_blocker.show()
+		left_blocker_collision.disabled = false;
+		left_blocker.modulate = Color.TRANSPARENT
+		tween.tween_property(left_blocker, "modulate", Color.WHITE, tween_durations)
+	elif event.is_action_released("BlockLeft"):
+		var tween = create_tween()
+		left_blocker_collision.disabled = true;
+		left_blocker.modulate = Color.WHITE
+		tween.tween_property(left_blocker, "modulate", Color.TRANSPARENT, tween_durations)
+		tween.tween_callback(left_blocker.hide)
+	elif event.is_action_pressed("BlockRight"):
+		var tween = create_tween()
+		right_blocker.show()
+		right_blocker_collision.disabled = false;
+		right_blocker.modulate = Color.TRANSPARENT
+		tween.tween_property(right_blocker, "modulate", Color.WHITE, tween_durations)
+	elif event.is_action_released("BlockRight"):
+		var tween = create_tween()
+		right_blocker_collision.disabled = true;
+		right_blocker.modulate = Color.WHITE
+		tween.tween_property(right_blocker, "modulate", Color.TRANSPARENT, tween_durations)
+		tween.tween_callback(right_blocker.hide)
