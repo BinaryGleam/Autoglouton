292d988387bd2667e6e40041e885362e770addea
Lukas TAMAYO <luknight@hotmail.fr>
2023-04-29 15:27:48 +0200

    One tween, no errors

--- Features/Pipe.gd
+++ Features/Pipe.gd
@@ -10,24 +10,25 @@ extends Node2D
 
 # Controlling pipe flow
 func _input(event):
-	var tween = create_tween()
-	if event.is_action_pressed("BlockLeft"):
-		left_blocker.show()
-		left_blocker.set_process(true)
-		left_blocker.modulate = Color.TRANSPARENT
-		tween.tween_property(left_blocker, "modulate", Color.WHITE, tween_durations)
-	elif event.is_action_released("BlockLeft"):
-		left_blocker.set_process(false)
-		left_blocker.modulate = Color.WHITE
-		tween.tween_property(left_blocker, "modulate", Color.TRANSPARENT, tween_durations)
-		tween.tween_callback(left_blocker.hide)
-	elif event.is_action_pressed("BlockRight"):
-		right_blocker.show()
-		right_blocker.set_process(true)
-		right_blocker.modulate = Color.TRANSPARENT
-		tween.tween_property(right_blocker, "modulate", Color.WHITE, tween_durations)
-	elif event.is_action_released("BlockRight"):
-		right_blocker.set_process(false)
-		right_blocker.modulate = Color.WHITE
-		tween.tween_property(right_blocker, "modulate", Color.TRANSPARENT, tween_durations)
-		tween.tween_callback(right_blocker.hide)
+	if event.is_action_type():
+		var tween = create_tween()
+		if event.is_action_pressed("BlockLeft"):
+			left_blocker.show()
+			left_blocker.set_process(true)
+			left_blocker.modulate = Color.TRANSPARENT
+			tween.tween_property(left_blocker, "modulate", Color.WHITE, tween_durations)
+		elif event.is_action_released("BlockLeft"):
+			left_blocker.set_process(false)
+			left_blocker.modulate = Color.WHITE
+			tween.tween_property(left_blocker, "modulate", Color.TRANSPARENT, tween_durations)
+			tween.tween_callback(left_blocker.hide)
+		elif event.is_action_pressed("BlockRight"):
+			right_blocker.show()
+			right_blocker.set_process(true)
+			right_blocker.modulate = Color.TRANSPARENT
+			tween.tween_property(right_blocker, "modulate", Color.WHITE, tween_durations)
+		elif event.is_action_released("BlockRight"):
+			right_blocker.set_process(false)
+			right_blocker.modulate = Color.WHITE
+			tween.tween_property(right_blocker, "modulate", Color.TRANSPARENT, tween_durations)
+			tween.tween_callback(right_blocker.hide)
