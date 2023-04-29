ec1904462d97d5167bc88aaacc55f609ba4d79d0
Lukas TAMAYO <luknight@hotmail.fr>
2023-04-29 15:13:54 +0200

    Tweened blockers, more thoughtfull inputs

--- Features/Pipe.gd
+++ Features/Pipe.gd
@@ -1,17 +1,33 @@
 class_name Pipe
 extends Node2D
 
+# Export variables
+@export_range(0.01, 1) var tween_durations : float = 0.05
+
 # Onready vars
-@onready var LeftBlocker = $PipeLeftBlocker
-@onready var RightBlocker = $PipeRightBlocker
+@onready var left_blocker = $PipeLeftBlocker
+@onready var right_blocker = $PipeRightBlocker
 
 # Controlling pipe flow
 func _input(event):
+	var tween = create_tween()
 	if event.is_action_pressed("BlockLeft"):
-		LeftBlocker.show()
-	if event.is_action_released("BlockLeft"):
-		LeftBlocker.hide()
-	if event.is_action_pressed("BlockRight"):
-		RightBlocker.show()
-	if event.is_action_released("BlockRight"):
-		RightBlocker.hide()
+		left_blocker.show()
+		left_blocker.set_process(true)
+		left_blocker.modulate = Color.TRANSPARENT
+		tween.tween_property(left_blocker, "modulate", Color.WHITE, tween_durations)
+	elif event.is_action_released("BlockLeft"):
+		left_blocker.set_process(false)
+		left_blocker.modulate = Color.WHITE
+		tween.tween_property(left_blocker, "modulate", Color.TRANSPARENT, tween_durations)
+		tween.tween_callback(left_blocker.hide)
+	elif event.is_action_pressed("BlockRight"):
+		right_blocker.show()
+		right_blocker.set_process(true)
+		right_blocker.modulate = Color.TRANSPARENT
+		tween.tween_property(right_blocker, "modulate", Color.WHITE, tween_durations)
+	elif event.is_action_released("BlockRight"):
+		right_blocker.set_process(false)
+		right_blocker.modulate = Color.WHITE
+		tween.tween_property(right_blocker, "modulate", Color.TRANSPARENT, tween_durations)
+		tween.tween_callback(right_blocker.hide)
